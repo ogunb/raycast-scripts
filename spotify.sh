842987409c10af23d127b1e757f5e0f66e390d40
Ogün Babacan <ogun.babacan@trendyol.com>
2021-03-04 22:13:53 +0300

    feat: add secrets.sh

--- spotify.sh
+++ spotify.sh
@@ -6,11 +6,12 @@
 # @raycast.icon https://play-lh.googleusercontent.com/dwyqtk9CgAaoXUcYB8pUHXRErF5A2Shd0UrZdBLkpAFx_e630aZahwN31HRZWNksIQ
 # @raycast.argument1 { "type": "text", "placeholder": "track" }
 
+source ./secrets.sh
+
 if ! pgrep -xq -- "Spotify"; then
 	open -a Spotify -jg
 fi
 
-SPOTIFY_TOKEN=
 URI=$(curl -G -X "GET" "https://api.spotify.com/v1/search" --data-urlencode "q=$*" --data-urlencode "type=track" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $SPOTIFY_TOKEN" | jq -r '.tracks.items | first | .uri')
 
 echo "URI is: $URI"
