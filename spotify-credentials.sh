717d4370fc90f533cb18e7ae94f41a8e18e71cce
Ogun Babacan <babacanogun@gmail.com>
2021-03-06 20:46:25 +0300

    fix: remove =

--- spotify-credentials.sh
+++ spotify-credentials.sh
@@ -7,5 +7,5 @@
 
 source ./secrets.sh
 
-access_token=$(curl -X "POST" -H "Authorization: Basic $SPOTIFY_APP_BASIC_TOKEN=" -d grant_type=client_credentials https://accounts.spotify.com/api/token | jq -r '.access_token')
+access_token=$(curl -X "POST" -H "Authorization: Basic $SPOTIFY_APP_BASIC_TOKEN" -d grant_type=client_credentials https://accounts.spotify.com/api/token | jq -r '.access_token')
 sed -i '' "s/SPOTIFY_TOKEN.*$/SPOTIFY_TOKEN=$access_token/g" secrets.sh
