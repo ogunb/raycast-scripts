dc899636a9b2ba315e09e162a69bd1594c72658c
Ogun Babacan <ogun.babacan@trendyol.com>
2022-03-06 15:46:32 +0300

    feat(): add breakout scripts

---
+++ zoom-grocery.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Breakout Grocery
+# @raycast.mode silent
+# @raycast.icon https://cdn.iconscout.com/icon/free/png-256/zoom-2471913-2050545.png
+
+source ./secrets.sh
+
+bash ./helpers/open-chrome.sh $BREAKOUT_GROCERY_ZOOM_URL
